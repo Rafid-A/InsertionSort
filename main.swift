b65eddc41dbcede22690527f0558067a7c5520fd
Rafid Ahmed <rafid.ahmed@student.allenisd.org>
2022-10-03 17:15:21 -0500

    Adding InsertionSort

---
+++ main.swift
@@ -0,0 +1,33 @@
+var unsortedIntegers = [String]()
+
+func readWords() -> [String] {
+var line : String?
+repeat {
+    line = readLine()
+    if line != nil {
+        unsortedIntegers.append(line!)
+    }
+} while line != nil
+return unsortedIntegers
+}
+
+func insertionSort(unsortedIntegers: [String]) -> [String] {
+
+    var sortingIntegers = readWords()
+    
+    for i in 1..<sortingIntegers.count { //The outer loop looks at each of the elements in the array in turn; this is what picks the top-most number from the pile. The outer loop starts at index 1, not 0. Moving the very first element from the pile to the sorted portion doesn't actually change anything, so I skipped it.
+            var j = i
+            while j > 0 && sortingIntegers[j - 1] > sortingIntegers[j] { //The inner loop looks at the element at position i. This is the number at the top of the pile, and it may be smaller than any of the previous elements. The inner loop steps backwards through the sorted array; every time it finds a previous number that is larger, it swaps them. When the inner loop completes, the beginning of the array is sorted again, and the sorted portion has grown by one element.
+               let tmp = sortingIntegers[j]
+               sortingIntegers[j] = sortingIntegers[j - 1]
+               sortingIntegers[j - 1] = tmp
+               j -= 1                 
+        }
+    }
+    return sortingIntegers
+}
+let sortedIntegers = insertionSort(unsortedIntegers: unsortedIntegers)
+
+for element in sortedIntegers {
+    print(element)
+} 
